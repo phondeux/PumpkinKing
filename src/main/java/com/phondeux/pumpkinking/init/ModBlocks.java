f33a94eeedec0fdbbd3e4670b5cae9476268212d
phondeux <fondue.daredevil@gmail.com>
2014-08-19 13:20:57 -0500

    Added GameRegistry flag for Items and Blocks

--- src/main/java/com/phondeux/pumpkinking/init/ModBlocks.java
+++ src/main/java/com/phondeux/pumpkinking/init/ModBlocks.java
@@ -2,11 +2,13 @@ package com.phondeux.pumpkinking.init;
 
 import com.phondeux.pumpkinking.block.BlockBone;
 import com.phondeux.pumpkinking.block.BlockPK;
+import com.phondeux.pumpkinking.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 /**
  * Created by Glenn on 8/19/2014.
  */
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks {
     public static final BlockPK boneblock = new BlockBone();
 
--- src/main/java/com/phondeux/pumpkinking/init/ModItems.java
+++ src/main/java/com/phondeux/pumpkinking/init/ModItems.java
@@ -2,11 +2,13 @@ package com.phondeux.pumpkinking.init;
 
 import com.phondeux.pumpkinking.item.ItemPK;
 import com.phondeux.pumpkinking.item.ItemSwizzle;
+import com.phondeux.pumpkinking.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 /**
  * Created by Glenn on 8/14/2014.
  */
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModItems {
     public static final ItemPK swizzle = new ItemSwizzle();
 
